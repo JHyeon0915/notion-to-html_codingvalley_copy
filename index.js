e4e40e425d505c933bfba4d8e1942c86ec7ef629
jungAh Han <truth.h406@gmail.com>
2023-11-03 18:52:33 +0900

    add: 불렛 모양 조건도 추가할수있도록 코드 추가

--- index.js
+++ index.js
@@ -5,13 +5,46 @@ function convertToOrderedList(inputText) {
   for (let i = 0; i < items.length; i += 2) {
     const itemContent = items[i + 1].trim();
     htmlOutput += `<li>${itemContent}</li>`;
+    console.log("ddd");
   }
   htmlOutput += `</ol>`;
   return htmlOutput;
 }
 
+function convertToUnorderedList(inputText) {
+  // '-'로 시작하는 부분을 찾아 분리합니다. 이때 결과 배열에는 빈 문자열이 포함되지 않습니다.
+  const items = inputText.split(/-\s+/).filter(Boolean);
+
+  // HTML 리스트 시작
+  let htmlOutput = `<h3>조건</h3><ul>`;
+
+  // 각 항목에 대해 리스트 항목을 생성합니다.
+  for (let i = 0; i < items.length; i++) {
+    // 각 항목의 앞뒤 공백을 제거합니다.
+    const itemContent = items[i].trim();
+
+    // 숫자로 시작하는 조건 항목을 처리합니다. 예: '1. 조건'을 '조건'으로 변경합니다.
+    const cleanedItemContent = itemContent.replace(/^\d+\.\s*/, "");
+
+    // HTML 리스트 항목을 추가합니다.
+    htmlOutput += `<li>${cleanedItemContent}</li>`;
+  }
+
+  // HTML 리스트 종료
+  htmlOutput += `</ul>`;
+
+  // 생성된 HTML 문자열을 반환합니다.
+  return htmlOutput;
+}
+
 function updateDOMWithOrderedList(inputText) {
   const convertedConditionsHtml = convertToOrderedList(inputText);
+  document.querySelector("#updatedParagraph").innerHTML +=
+    convertedConditionsHtml;
+}
+
+function updateDOMWithUnorderedList(inputText) {
+  const convertedConditionsHtml = convertToUnorderedList(inputText);
   document.querySelector("#updatedParagraph").innerHTML +=
     convertedConditionsHtml;
 }
@@ -23,8 +56,16 @@ function convert() {
   document.getElementsByTagName("p")[0].innerHTML = text;
   document.getElementsByTagName("img")[0].src = imgUrl;
 
-  const inputCondition = document.getElementById("conditionInput").value;
-  if (inputCondition) updateDOMWithOrderedList(inputCondition);
+  const inpuOrderedCondition = document.getElementById(
+    "orderedConditionInput"
+  ).value;
+  if (inpuOrderedCondition) updateDOMWithOrderedList(inpuOrderedCondition);
+
+  const inputUnorderedCondition = document.getElementById(
+    "unorderedConditionInput"
+  ).value;
+  if (inputUnorderedCondition)
+    updateDOMWithUnorderedList(inputUnorderedCondition);
 
   //결과 합치기
   const updatedParagraph =
@@ -60,6 +101,11 @@ function reset() {
   document.querySelector("#result").innerText = "";
   if (document.getElementsByTagName("ol")[0]) {
     document.getElementsByTagName("ol")[0].remove();
+    document.getElementsByTagName("h3")[0].remove();
+  }
+
+  if (document.getElementsByTagName("ul")[0]) {
+    document.getElementsByTagName("ul")[0].remove();
     document.getElementsByTagName("h3")[0].remove();
   }
 
